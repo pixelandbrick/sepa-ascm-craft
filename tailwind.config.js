3dc49e581735f1e814586c94bc4c789fb0a7b003
Ryan Lewis <ryan@rmlew.com>
2024-04-17 14:12:43 -0400

    style updates and UAT bug fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,21 @@ module.exports = {
       minHeight: {
         'screenh' : '100vh',
       },
+      colors: {
+        'umbra': '#1d1d1d',
+        'wolf': '#292929',
+        'vault': '#727272',
+        'shaving': '#E2E5E7',
+        'errigal': '#f2f2f2',
+        'asparagus': '#75AE4C',
+        'money': '#82C341',
+        'grass': '#99C47C',
+        'ficus': '#006B36',
+        'storm': '#343F48',
+        'fjord': '#495965',
+        'mercury': '#E9ECEF',
+        'azure': '#007BFF',
+      },
     },
     fontFamily: {
       sans: ['adelle-sans', 'sans-serif'],
